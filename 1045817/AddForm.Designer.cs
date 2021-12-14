687103efe862d54b8305d89b46ad4962d7362dfa
Toynmaker <96136024+Toynmaker@users.noreply.github.com>
2021-12-14 12:52:01 -0400

    Add files via upload

---
+++ 1045817/AddForm.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace ExampleIV._1045817
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.intDistancia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.intTemperatura = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.floatGravedad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.intCantidad = new System.Windows.Forms.TextBox();
+            this.cboSuperficie = new System.Windows.Forms.ComboBox();
+            this.cboClasificacion = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.intRadio = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.intEdad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.intDistancia);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.intTemperatura);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.floatGravedad);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.intCantidad);
+            this.groupBox1.Controls.Add(this.cboSuperficie);
+            this.groupBox1.Controls.Add(this.cboClasificacion);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.intRadio);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtColor);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.intEdad);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(727, 244);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "NUEVO ASTRO";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(565, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 102);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(562, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(154, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Distancia del Sol (Millones Km):";
+            // 
+            // intDistancia
+            // 
+            this.intDistancia.Location = new System.Drawing.Point(565, 150);
+            this.intDistancia.Name = "intDistancia";
+            this.intDistancia.Size = new System.Drawing.Size(146, 20);
+            this.intDistancia.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(380, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Temperatura Media (Celcius):";
+            // 
+            // intTemperatura
+            // 
+            this.intTemperatura.Location = new System.Drawing.Point(383, 150);
+            this.intTemperatura.Name = "intTemperatura";
+            this.intTemperatura.Size = new System.Drawing.Size(146, 20);
+            this.intTemperatura.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(380, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Gravedad (m/s^2)";
+            // 
+            // floatGravedad
+            // 
+            this.floatGravedad.Location = new System.Drawing.Point(383, 101);
+            this.floatGravedad.Name = "floatGravedad";
+            this.floatGravedad.Size = new System.Drawing.Size(146, 20);
+            this.floatGravedad.TabIndex = 18;
+            this.floatGravedad.TextChanged += new System.EventHandler(this.floatGravedad_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(380, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Cantidad de Lunas:";
+            // 
+            // intCantidad
+            // 
+            this.intCantidad.Location = new System.Drawing.Point(383, 54);
+            this.intCantidad.Name = "intCantidad";
+            this.intCantidad.Size = new System.Drawing.Size(146, 20);
+            this.intCantidad.TabIndex = 16;
+            // 
+            // cboSuperficie
+            // 
+            this.cboSuperficie.FormattingEnabled = true;
+            this.cboSuperficie.Items.AddRange(new object[] {
+            "Solido",
+            "Liquido",
+            "Gaseoso",
+            "Plasma"});
+            this.cboSuperficie.Location = new System.Drawing.Point(18, 100);
+            this.cboSuperficie.Name = "cboSuperficie";
+            this.cboSuperficie.Size = new System.Drawing.Size(121, 21);
+            this.cboSuperficie.TabIndex = 15;
+            // 
+            // cboClasificacion
+            // 
+            this.cboClasificacion.FormattingEnabled = true;
+            this.cboClasificacion.Items.AddRange(new object[] {
+            "Estrella",
+            "Planeta",
+            "Satélite",
+            "Cometa"});
+            this.cboClasificacion.Location = new System.Drawing.Point(18, 54);
+            this.cboClasificacion.Name = "cboClasificacion";
+            this.cboClasificacion.Size = new System.Drawing.Size(121, 21);
+            this.cboClasificacion.TabIndex = 3;
+            this.cboClasificacion.SelectedIndexChanged += new System.EventHandler(this.cboClasificacion_SelectedIndexChanged);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(18, 193);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 28);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(192, 193);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(125, 28);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(189, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Radio (Millones Km):";
+            // 
+            // intRadio
+            // 
+            this.intRadio.Location = new System.Drawing.Point(192, 150);
+            this.intRadio.Name = "intRadio";
+            this.intRadio.Size = new System.Drawing.Size(146, 20);
+            this.intRadio.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Color:";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(18, 150);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(121, 20);
+            this.txtColor.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Edad (Millones):";
+            // 
+            // intEdad
+            // 
+            this.intEdad.Location = new System.Drawing.Point(192, 101);
+            this.intEdad.Name = "intEdad";
+            this.intEdad.Size = new System.Drawing.Size(146, 20);
+            this.intEdad.TabIndex = 7;
+            this.intEdad.TextChanged += new System.EventHandler(this.intEdad_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Superficie:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clasificación:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(192, 54);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(146, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 327);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "AddForm";
+            this.Text = "AddForm";
+            this.Load += new System.EventHandler(this.AddForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboSuperficie;
+        private System.Windows.Forms.ComboBox cboClasificacion;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox intRadio;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox intEdad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox intDistancia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox intTemperatura;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox floatGravedad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox intCantidad;
+    }
+}
---
+++ 1045817/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExampleIV._1045817
+{
+    public partial class MainForm : Form
+    {
+        nov_jan_2021Entities db = new nov_jan_2021Entities();
+        public MainForm()
+        {
+            InitializeComponent();
+            GetRecords();
+        }
+
+        private void GetRecords()
+        {
+            var astros = db.C1045817_Astros.ToList();
+            dgvRegistros.DataSource = astros;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            var oForm = new AddForm();
+            oForm.ShowDialog();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
